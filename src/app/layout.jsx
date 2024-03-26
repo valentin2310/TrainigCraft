8466713bc5eef03aa238f9be60cfe1a832c6d449
ZheiosAC <elvaliac23@gmail.com>
2024-03-26 18:55:25 +0100

    nextui library

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import 'remixicon/fonts/remixicon.css'
+import { Providers } from "@/app/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+        </body>
     </html>
   );
 }
