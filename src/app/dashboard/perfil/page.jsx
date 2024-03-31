bdca22f5884c57d85fc409af16afb6ac1341ddc9
ZheiosAC <elvaliac23@gmail.com>
2024-03-31 21:31:02 +0200

    comprobar si existe usuario

--- src/app/dashboard/perfil/page.jsx
+++ src/app/dashboard/perfil/page.jsx
@@ -15,11 +15,15 @@ export default function Page() {
                     <Button color="primary" variant="ghost"> <i className="ri-edit-2-line m-2"></i>Editar</Button>
                 </div>
                 <div className="flex gap-10 items-center">
-                    <Avatar isBordered color="primary" src={user.photoURL} className="w-30 h-30"></Avatar>
-                    <div className="border-l-4 border-primary py-2 px-4">
-                        <p className="text-3xl font-bold">{user.displayName}</p>
-                        <p className="text-xl">{user.email}</p>
-                    </div>
+                    {user && 
+                        <>
+                            <Avatar isBordered color="primary" src={user.photoURL} className="w-30 h-30"></Avatar>
+                            <div className="border-l-4 border-primary py-2 px-4">
+                                <p className="text-3xl font-bold">{user.displayName}</p>
+                                <p className="text-xl">{user.email}</p>
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
         </>
