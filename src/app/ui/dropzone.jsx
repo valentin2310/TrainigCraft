aa37f83a2d76c7a8b04ea0b5097b15c29c835d0a
ZheiosAC <elvaliac23@gmail.com>
2024-04-08 13:16:26 +0200

    dropzone

---
+++ src/app/ui/dropzone.jsx
@@ -0,0 +1,29 @@
+'use client'
+
+import React from 'react';
+import {useDropzone} from 'react-dropzone';
+
+import '@/styles/dropzone.css'
+
+export default function MyDropzone(props) {
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+  
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  return (
+    <section className="container">
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+    </section>
+  );
+}
