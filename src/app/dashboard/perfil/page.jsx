a896b7c2329411191cfc8508ad937c4628e7043d
Valentin Andrei Culea <valentin@regisof.com>
2024-05-30 11:18:53 +0200

    ajustar pantalla de perfil

--- src/app/dashboard/perfil/page.jsx
+++ src/app/dashboard/perfil/page.jsx
@@ -2,7 +2,7 @@
 
 import { UserContext } from "@/app/providers"
 import MisObjetivos from "@/app/ui/dashboard/mis-objetivos"
-import { Button, Avatar } from "@nextui-org/react"
+import { Button, Avatar, Link } from "@nextui-org/react"
 import { Suspense, useContext } from "react"
 
 export default function Page() {
@@ -14,15 +14,25 @@ export default function Page() {
             <div className="mb-10">
                 <div className="flex justify-between px-2">
                     <p className="font-semibold text-xl text-primary">Mi perfil</p>
-                    <Button color="primary" variant="ghost"> <i className="ri-edit-2-line m-2"></i>Editar</Button>
+                    <Link href="#">
+                        <Button className="sm:hidden rounded-full" size="sm" variant="flat" color="primary" isIconOnly startContent={<i className="ri-edit-2-line"></i>} />
+                        <Button className="hidden sm:flex" variant="flat" color="primary" startContent={<i className="ri-edit-2-line text-xl"></i>}>
+                            <span>Editar</span>
+                        </Button>
+                    </Link>
                 </div>
-                <div className="flex gap-10 items-center">
+                <div className="flex flex-col md:flex-row gap-3 md:gap-10 items-center mt-3 md:mt-0">
                     {user && 
                         <>
                             <Avatar isBordered color="primary" src={user.photoUrl} className="w-30 h-30"></Avatar>
-                            <div className="border-l-4 border-primary py-2 px-4">
-                                <p className="text-2xl"><span className="text-3xl font-bold">@{user.usuario}<span className="text-primary me-2">/</span></span>{user.nombre}</p>
-                                <p className="text-xl">{user.correo}</p>
+                            <div className="border-b-2 md:border-b-0 md:border-l-4 border-primary py-2 px-4 text-center md:text-start">
+                                <div className="text-lg flex flex-wrap justify-center md:justify-normal sm:text-xl md:text-2xl">
+                                    <span className="text-xl sm:text-2xl md:text-3xl font-bold">@{user.usuario}
+                                        <span className="text-primary font-semibold md:font-bold me-2">/</span>
+                                    </span>
+                                    {user.nombre}
+                                </div>
+                                <p className="text-medium sm:text-lg md:text-xl">{user.correo}</p>
                             </div>
                         </>
                     }
