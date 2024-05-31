135f9061c089eecbdcbb38a9deacd6db08ec3027
Valentin Andrei Culea <valentin@regisof.com>
2024-05-31 11:45:35 +0200

    mensaje por defecto si no hay registros

--- src/app/ui/dashboard/mis-objetivos.jsx
+++ src/app/ui/dashboard/mis-objetivos.jsx
@@ -2,7 +2,7 @@
 
 import { use, useEffect } from "react"
 import { UserContext } from "@/app/providers"
-import { Button, useDisclosure } from "@nextui-org/react"
+import { Button, Link, useDisclosure } from "@nextui-org/react"
 import ObjetivoModalForm from "@/app/ui/objetivos/modal-form"
 import ScrollObjetivos from "@/app/ui/objetivos/scroll-objetivos"
 import { useObjetivos } from "@/app/stores/use-objetivos"
@@ -28,19 +28,26 @@ export default function MisObjetivos({ nObjetivos = 10 }) {
              <div className="w-full flex justify-between items-center mb-2">
                 <p className="text-lg xl:text-xl text-primary font-semibold"><i className="ri-focus-2-line me-2"></i>Mis objetivos</p>
                 <div className="flex gap-1 sm:gap-2">
-                    <Button className="sm:hidden rounded-full" size="sm" onPress={onOpen} variant="flat" color="primary" isIconOnly startContent={<i className="ri-add-circle-line"></i>} />
-                    <Button className="sm:hidden rounded-full" size="sm" variant="flat" color="primary" isIconOnly startContent={<i className="ri-eye-line"></i>} />
+                    <Button className="sm:hidden rounded-full" size="sm" onPress={onOpen} variant="flat" color="primary" isIconOnly startContent={<i className="ri-file-add-line"></i>} />
                     
-                    <Button className="hidden sm:flex" onPress={onOpen} variant="flat" color="primary" startContent={<i className="ri-add-circle-line text-lg"></i>}>
+                    <Button className="hidden sm:flex" onPress={onOpen} variant="flat" color="primary" startContent={<i className="ri-file-add-line text-lg"></i>}>
                         <span>Añadir</span>
                     </Button>
-                    <Button className="hidden sm:flex" variant="flat" color="primary" startContent={<i className="ri-eye-line text-lg"></i>}>
-                        <span>Ver todas</span>
-                    </Button>
+
+                    <Link href="dashboard/perfil">
+                        <Button className="sm:hidden rounded-full" size="sm" variant="flat" color="primary" isIconOnly startContent={<i className="ri-eye-line"></i>} />
+                        <Button className="hidden sm:flex" variant="flat" color="primary" startContent={<i className="ri-eye-line text-lg"></i>}>
+                            <span>Ver todas</span>
+                        </Button>
+                    </Link>
                 </div>
              </div>
             
-            <ScrollObjetivos lista={objetivos} />
+            {(objetivos && objetivos.length > 0) && 
+                <ScrollObjetivos lista={objetivos} />
+                ||
+                <p className="text-sm italic px-2"><i className="ri-information-2-line text-primary me-2"></i>No tienes ningún objetivo</p>
+            }
 
             {user && 
                 <ObjetivoModalForm 
